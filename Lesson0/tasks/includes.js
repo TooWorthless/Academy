48952a722800f47452fe88db58b8e2c92206ac6b
TooWorthless <malikovvadime@gmail.com>
2025-01-04 21:06:57 +0200

    update includes method

--- Lesson0/tasks/includes.js
+++ Lesson0/tasks/includes.js
@@ -1,13 +1,8 @@
 export default function includes(text, matchStr, index = 0) {
-
-    if (typeof text !== "string" || typeof matchStr !== "string") {
-        throw new TypeError("Params type error");
-    }
+    let matchIndex = 0;
+    let startCheck = false;
 
     index = index < 0 ? 0 : index;
-
-    let matchIndex = 0;
-    let startCheck = false; 
 
     for (let i = 0; i < text.length; i++) {
 
